993fdf28f5c3f033212285e67e7a6a017f439f24
Kumar Sai Uggina <uggina.kumarsai@gmail.com>
2025-01-01 22:57:42 +0530

    Started with redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
+import "./App.css";
 
-import { Routes } from 'react-router-dom';
+import { Routes } from "react-router-dom";
 
 function App() {
-  return <>
-    <h1>App</h1>
-    <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
-    </Routes>
-  </>;
+  return (
+    <>
+      <h1>App</h1>
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+    </>
+  );
 }
 
 export default App;
