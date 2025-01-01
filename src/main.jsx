48206ddbd6cdc84974a15b1b45418165a4626922
Kumar Sai Uggina <uggina.kumarsai@gmail.com>
2025-01-01 22:37:13 +0530

    Configured react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@
 import "./index.css";
 
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
@@ -10,5 +11,6 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Toaster/>
   </BrowserRouter>
 );
